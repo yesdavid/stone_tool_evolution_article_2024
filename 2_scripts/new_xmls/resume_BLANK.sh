20fa4811d2e4663d9fc17a8cf72ff625e2423fa4
yesdavid <30009559+yesdavid@users.noreply.github.com>
2023-06-28 09:22:18 +0200

    templates for run and resume

---
+++ 2_scripts/new_xmls/resume_BLANK.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#
+# allocate nodes
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#
+# job name 
+#PBS -N RUN_PLACEHOLDER_CURRENT_SCRIPT_PLACEHOLDER
+#
+# first non-empty non-comment line ends PBS options
+
+# jobs always start in $HOME 
+# change to the working directory
+cd /home/woody/gwpa/gwpa007h/BEAST2_contraband/
+
+# run your job
+# java -jar contraband.jar -statefile CURRENT_FOLDER_PLACEHOLDER/CURRENT_SCRIPT_PLACEHOLDER.state CURRENT_FOLDER_PLACEHOLDER/CURRENT_SCRIPT_PLACEHOLDER.xml
+
+# resume your job
+java -jar contraband.jar -statefile CURRENT_FOLDER_PLACEHOLDER/CURRENT_SCRIPT_PLACEHOLDER.state -resume CURRENT_FOLDER_PLACEHOLDER/CURRENT_SCRIPT_PLACEHOLDER.xml
